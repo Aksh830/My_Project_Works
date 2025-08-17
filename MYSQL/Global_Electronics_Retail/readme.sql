38f4acf3ffc028a7caded73fb9b9fca2d80c0526
Aksh Shah <akshshah666@gmail.com>
2025-08-17 14:12:14 +0100

    Update readme.sql

--- MYSQL/Global_Electronics_Retail/readme.sql
+++ MYSQL/Global_Electronics_Retail/readme.sql
@@ -1,110 +1,120 @@
 -- 1. Data Cleaning Exchange_rates table
-select * from Global_electronics_retail.exchange_rates;
+SELECT * FROM Global_electronics_retail.exchange_rates;
 
 -- Bringing Date into SQL recognised format
-SELECT *, str_to_date(`Date`,'%m/%d/%Y') as new_date FROM Global_electronics_retail.exchange_rates;
-update Global_electronics_retail.exchange_rates set `date` = str_to_date(`Date`,'%m/%d/%Y');
+SELECT *, str_to_date(`Date`,'%m/%d/%Y') as new_date 
+FROM Global_electronics_retail.exchange_rates;
 
-alter table Global_electronics_retail.exchange_rates modify `Date` date;
+UPDATE Global_electronics_retail.exchange_rates 
+SET `Date` = str_to_date(`Date`,'%m/%d/%Y');
 
--- checking for duplicates (None Found)
-SELECT `date`,`Exchange`,count(*) dup_col FROM Global_electronics_retail.exchange_rates
-group by `date`, `Exchange` 
-having dup_col >1;
+ALTER TABLE Global_electronics_retail.exchange_rates 
+MODIFY `Date` DATE;
 
--- checking for null values (None Found)
+-- Checking for duplicates (None Found)
+SELECT `Date`, `Exchange`, COUNT(*) dup_col 
+FROM Global_electronics_retail.exchange_rates
+GROUP BY `Date`, `Exchange` 
+HAVING dup_col > 1;
+
+-- Checking for null values (None Found)
 SELECT * FROM Global_electronics_retail.exchange_rates 
-where date = null or '';
+WHERE `Date` IS NULL OR `Date` = '';
 
 SELECT * FROM Global_electronics_retail.exchange_rates 
-where currency = null or '';
+WHERE `Currency` IS NULL OR `Currency` = '';
 
 SELECT * FROM Global_electronics_retail.exchange_rates
-where `Exchange` = null or '';
+WHERE `Exchange` IS NULL OR `Exchange` = '';
 
 -- 2. Data Cleaning Customers table
 SELECT * FROM Global_electronics_retail.customers
-order by CustomerKey;
+ORDER BY CustomerKey;
 
--- Checking for Duplicate Values (CustomerKey is Primary Key, No Duplicates)
-SELECT CustomerKey,count(*) as dup_cus FROM Global_electronics_retail.customers
-group by CustomerKey
-having dup_cus > 1;
+-- Checking for duplicate values (CustomerKey is Primary Key, No Duplicates)
+SELECT CustomerKey, COUNT(*) as dup_cus 
+FROM Global_electronics_retail.customers
+GROUP BY CustomerKey
+HAVING dup_cus > 1;
 
 -- Bringing Birthday into SQL recognised format 
-update Global_electronics_retail.customers
-set Birthday = str_to_date(Birthday,'%m/%d/%Y');
+UPDATE Global_electronics_retail.customers
+SET Birthday = STR_TO_DATE(Birthday,'%m/%d/%Y');
 
--- assigning data type
-alter table Global_electronics_retail.customers
-modify Birthday date;
+-- Assigning data type
+ALTER TABLE Global_electronics_retail.customers
+MODIFY Birthday DATE;
 
--- 3. Data cleaning sales Table
+-- 3. Data Cleaning Sales table
 SELECT * FROM Global_electronics_retail.sales;
 
 -- Bringing Date into SQL recognised format
-update Global_electronics_retail.sales
-set `order date` = str_to_date(`order date`,'%m/%d/%Y');
+UPDATE Global_electronics_retail.sales
+SET `Order Date` = STR_TO_DATE(`Order Date`, '%m/%d/%Y');
 
 -- Updating '' values to null to help change delivery dates into date format 
-update Global_electronics_retail.sales
-set `Delivery Date` = null
-where `Delivery Date` = '';
+UPDATE Global_electronics_retail.sales
+SET `Delivery Date` = NULL
+WHERE `Delivery Date` = '';
 
-update Global_electronics_retail.sales
-set `Delivery Date` = str_to_date(`Delivery Date`,'%m/%d/%Y');
+UPDATE Global_electronics_retail.sales
+SET `Delivery Date` = STR_TO_DATE(`Delivery Date`, '%m/%d/%Y');
 
--- checking for duplicate values (8 duplicate values found/deleted)
-SELECT `Order Number`, `Order Date`, `Delivery Date`, CustomerKey, StoreKey, ProductKey, Quantity, `Currency Code`, count(*) as dup_ord FROM Global_electronics_retail.sales
-group by `Order Number`, `Order Date`, `Delivery Date`, CustomerKey, StoreKey, ProductKey, Quantity, `Currency Code`;
--- created a CTE to ensure that duplicates exist
+-- Checking for duplicate values (8 duplicate values found/deleted)
+SELECT `Order Number`, `Order Date`, `Delivery Date`, CustomerKey, StoreKey, ProductKey, Quantity, `Currency Code`, COUNT(*) AS dup_ord 
+FROM Global_electronics_retail.sales
+GROUP BY `Order Number`, `Order Date`, `Delivery Date`, CustomerKey, StoreKey, ProductKey, Quantity, `Currency Code`;
+
+-- Created a CTE to ensure that duplicates exist
 WITH duplicates AS (
     SELECT 
         *,
         ROW_NUMBER() OVER (PARTITION BY `Order Number`, `Order Date`, `Delivery Date`, CustomerKey, StoreKey, ProductKey, Quantity, `Currency Code` ) AS rn
     FROM Global_electronics_retail.sales
 )
- select * from duplicates
- WHERE rn > 1;
- 
--- created duplicate table to keep original data and have deletable table 
- create table sales_staging
- as (
+SELECT * FROM duplicates
+WHERE rn > 1;
+
+-- Created duplicate table to keep original data and have deletable table 
+CREATE TABLE sales_staging AS (
     SELECT 
         *,
         ROW_NUMBER() OVER (PARTITION BY `Order Number`, `Order Date`, `Delivery Date`, CustomerKey, StoreKey, ProductKey, Quantity, `Currency Code` ) AS rn
     FROM Global_electronics_retail.sales
 );
+
 SELECT * FROM Global_electronics_retail.sales_staging;
 
--- deleting duplicates newly created table
-delete from Global_electronics_retail.sales_staging
-where rn >1;
+-- Deleting duplicates from newly created table
+DELETE FROM Global_electronics_retail.sales_staging
+WHERE rn > 1;
 
--- dropped extra column
-alter table Global_electronics_retail.sales_staging
-drop column rn;
+-- Dropped extra column
+ALTER TABLE Global_electronics_retail.sales_staging
+DROP COLUMN rn;
 
--- 4. data cleaning Stores table
-
+-- 4. Data Cleaning Stores table
 SELECT * FROM Global_electronics_retail.stores;
--- removing extra row with all null values
-delete FROM Global_electronics_retail.stores
-where StoreKey = null;
--- Checking for duplicated (None Found)
-select storeKey, count(*) as dup
-from Global_electronics_retail.stores
-group by storeKey
-having dup >1;
 
+-- Removing extra row with all null values
+DELETE FROM Global_electronics_retail.stores
+WHERE StoreKey IS NULL;
+
+-- Checking for duplicates (None Found)
+SELECT StoreKey, COUNT(*) AS dup
+FROM Global_electronics_retail.stores
+GROUP BY StoreKey
+HAVING dup > 1;
 
 -- 5. Data Cleaning Products table
 SELECT * FROM Global_electronics_retail.products;
--- Checking for duplicates (None Found)
-SELECT ProductKey,count(*) as dup FROM Global_electronics_retail.products
-group by ProductKey
-having dup >1;
--- checking for null values (None Found)
-SELECT * FROM Global_electronics_retail.products
-where ProductKey = '' or null;
 
+-- Checking for duplicates (None Found)
+SELECT ProductKey, COUNT(*) AS dup 
+FROM Global_electronics_retail.products
+GROUP BY ProductKey
+HAVING dup > 1;
+
+-- Checking for null values (None Found)
+SELECT * FROM Global_electronics_retail.products
+WHERE ProductKey IS NULL OR ProductKey = '';
